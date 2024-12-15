59254dac28c3e87fd13981b77d59bf620ecc8b7e
Santiago Eduardo Poblete Talamante <a01254609@tec.mx>
2024-12-15 12:49:08 -0700

    project start

---
+++ webBasketball/src/App.tsx
@@ -0,0 +1,11 @@
+import LandingPage from './pages/LandingPage'
+import './App.css'
+
+function App() {
+
+  return (
+    <LandingPage />
+  )
+}
+
+export default App
---
+++ webBasketball/src/pages/LandingPage.tsx
@@ -0,0 +1,10 @@
+function LandingPage() {
+  return (
+    <div>
+      <h1>Welcome to Web Basketball</h1>
+      
+    </div>
+  );
+}
+
+export default LandingPage;
